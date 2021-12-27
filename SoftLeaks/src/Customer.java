eb404e4659c15bb0bf1faa2a2c1d7f13b94e8be0
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-12-28 04:30:54 +0530

    soft leaks

---
+++ SoftLeaks/src/Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+
+    private int id;
+    private String name;
+
+    public Customer(String name) {
+        super();
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toString() {
+        return id + " : " + name;
+    }
+
+
+}
+
